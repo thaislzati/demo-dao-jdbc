088758ff00199d9dbbe653f461c9466da48f1890
Thais Silva <thais.lza.ti@gmail.com>
2024-06-06 14:39:19 -0300

    Department entity class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import model.entities.Department;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Department department = new Department(1, "Books");
+        System.out.println(department);
     }
 }
