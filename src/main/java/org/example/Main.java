96a21cc3171301d86a81ce0ae7b0f8a4094b7215
Thais Silva <thais.lza.ti@gmail.com>
2024-06-06 14:53:40 -0300

    Seller entity class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,15 @@
 package org.example;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
         Department department = new Department(1, "Books");
         System.out.println(department);
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(),3000.,department);
+        System.out.println(seller);
     }
 }
