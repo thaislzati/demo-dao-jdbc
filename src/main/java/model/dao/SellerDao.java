4c886801b36dd5c26e7d21623e4e2d557d0ad4be
Thais Silva <thais.lza.ti@gmail.com>
2024-06-06 15:39:39 -0300

    Update Interface

--- src/main/java/model/dao/SellerDao.java
+++ src/main/java/model/dao/SellerDao.java
@@ -1,2 +1,13 @@
-package model.dao;public interface ProductDao {
+package model.dao;
+
+import model.entities.Seller;
+
+import java.util.List;
+
+public interface SellerDao {
+    void insert (Seller seller);
+    void update (Seller seller);
+    void deleteById (Integer id);
+    Seller findById (Integer id);
+    List<Seller> findAll();
 }
